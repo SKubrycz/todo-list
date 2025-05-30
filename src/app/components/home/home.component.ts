d21636f1f65333efb99ca46f6cf20f5a6937cb16
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2025-05-30 10:40:49 +0200

    Update home and note components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-notes',
-  imports: [],
+  selector: 'app-home',
+  imports: [RouterLink, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
