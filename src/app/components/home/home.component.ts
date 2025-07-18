f037d617ee85d36a86cdb7b472b65ecebc70d2da
Sebastian Kubrycz <sebastian.kubrycz@gmail.com>
2025-07-18 13:45:12 +0200

    Added animation for scroll button & moved some keyframes to styles.css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -57,18 +57,57 @@ export class HomeComponent implements OnInit, AfterViewInit {
       iconClass: 'pi pi-check',
     },
   ];
+  protected scrollButton: HTMLElement | null = null;
+  protected scrollAnimationEnd: (() => void) | null = null;
   protected isAtBottom = false;
 
   ngOnInit() {
     this.joinedWords = this.joinedWords + ' ' + this.joinedWords;
+    this.scrollButton = this.elementRef.nativeElement.querySelector(
+      '#scroll-down-button'
+    ) as HTMLElement;
   }
 
   ngAfterViewInit() {}
 
   @HostListener('window:scroll', ['$event'])
+  @HostListener('')
   checkIsAtBottom() {
     this.isAtBottom =
       window.scrollY + window.innerHeight >= document.body.scrollHeight;
+
+    if (!this.scrollButton) return;
+    if (this.isAtBottom) {
+      this.scrollButton.style.animation = `800ms fadeOut 1`;
+
+      if (this.scrollAnimationEnd) {
+        this.scrollButton.removeEventListener(
+          'animationend',
+          this.scrollAnimationEnd
+        );
+        this.scrollAnimationEnd = null;
+      }
+
+      this.scrollAnimationEnd = () => {
+        if (this.scrollButton) this.scrollButton.style.display = 'none';
+
+        if (this.scrollButton && this.scrollAnimationEnd) {
+          this.scrollButton.removeEventListener(
+            'animationend',
+            this.scrollAnimationEnd
+          );
+          this.scrollAnimationEnd = null;
+        }
+      };
+
+      this.scrollButton.addEventListener(
+        'animationend',
+        this.scrollAnimationEnd
+      );
+    } else {
+      this.scrollButton.style.animation = '2000ms bounceTranslated infinite';
+      this.scrollButton.style.display = 'flex';
+    }
   }
 
   scrollDown(e: MouseEvent) {
