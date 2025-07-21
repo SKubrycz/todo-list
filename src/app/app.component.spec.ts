c3b14b9070b70fbdb70b1901d9fd873e65249fd1
Sebastian Kubrycz <sebastian.kubrycz@gmail.com>
2025-07-21 14:07:42 +0200

    Adjusted AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { DarkModeComponent } from './components/dark-mode/dark-mode.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todo-list' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-list');
-  });
+  it('should render router-outlet and app-dark-mode', () => {
+    const routerOutletElement = fixture.debugElement.query(
+      By.directive(RouterOutlet)
+    );
+    expect(routerOutletElement).toBeDefined();
+    expect(routerOutletElement).toBeTruthy();
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo-list');
+    const appDarkModeElement = fixture.debugElement.query(
+      By.directive(DarkModeComponent)
+    );
+    expect(appDarkModeElement).toBeDefined();
+    expect(appDarkModeElement).toBeTruthy();
   });
 });
