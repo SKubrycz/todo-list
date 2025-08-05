ea0955721a1b91f495922c27f625eb355e407f86
Sebastian Kubrycz <sebastian.kubrycz@gmail.com>
2025-08-05 23:43:37 +0200

    Added dark-mode tests

--- src/dark-mode/dark-mode.component.spec.ts
+++ src/dark-mode/dark-mode.component.spec.ts
@@ -1,22 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DarkModeComponent } from './dark-mode.component';
+import { DOCUMENT } from '@angular/common';
 
 describe('DarkModeComponent', () => {
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [DarkModeComponent],
-        }).compileComponents();
-    })
+  let fixture: ComponentFixture<DarkModeComponent>;
+  let darkModeComponent: DarkModeComponent;
+  let document: Document;
 
-    it('should create dark-mode.component', () => {
-        const fixture = TestBed.createComponent(DarkModeComponent);
-        const darkModeComponent = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DarkModeComponent],
+    }).compileComponents();
 
-        expect(darkModeComponent).toBeTruthy();
-    })
+    fixture = TestBed.createComponent(DarkModeComponent);
+    darkModeComponent = fixture.componentInstance;
+    document = TestBed.inject(DOCUMENT);
+  });
 
-    it('test', () => {
-        expect(true).toBeTrue();
-    })
+  it('should create dark-mode.component', () => {
+    fixture.detectChanges();
+
+    expect(darkModeComponent).toBeTruthy();
+  });
+
+  it('should toggle background color class', () => {
+    fixture.detectChanges();
+
+    darkModeComponent['darkMode'] = true;
+    darkModeComponent['updateDarkMode'].apply(darkModeComponent, [false]);
+
+    expect(
+      document.documentElement.classList.contains('my-app-light')
+    ).toBeTrue();
+    expect(
+      document.documentElement.classList.contains('my-app-dark')
+    ).toBeFalse();
+
+    darkModeComponent['updateDarkMode'].apply(darkModeComponent, [true]);
+
+    expect(
+      document.documentElement.classList.contains('my-app-light')
+    ).toBeFalse();
+    expect(
+      document.documentElement.classList.contains('my-app-dark')
+    ).toBeTrue();
+  });
 });
