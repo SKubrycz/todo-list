67dfccbaea86bc3ca9323af8262953dc2a88f445
Sebastian Kubrycz <sebastian.kubrycz@gmail.com>
2025-09-17 12:54:59 +0200

    Added routes for frontend auth & prepared /register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { NotesComponent } from '../notes-list/notes/notes.component';
+import { RegisterComponent } from '../auth/register/register.component';
+import { LoginComponent } from '../auth/login/login.component';
+import { VerifyComponent } from '../auth/verify/verify.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'verify', component: VerifyComponent },
   { path: 'notes', component: NotesComponent },
 ];
---
+++ frontend/src/auth/register/register.component.ts
@@ -0,0 +1,57 @@
+import { JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule,
+    JsonPipe,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent implements OnInit {
+  protected registerForm!: FormGroup;
+
+  private fb = inject(FormBuilder);
+
+  ngOnInit() {
+    this.initializeNoteForm();
+  }
+
+  private initializeNoteForm() {
+    this.registerForm = this.fb.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.{3,50}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'
+          ),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+-]).*'
+          ),
+        ],
+      ],
+    });
+  }
+}
